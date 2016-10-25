d1a6b4391c87d441ca3dd18cbbb2e253589af90b
Stefan Bauer <sauberzof@gmail.com>
2016-10-25 10:16:02 +0200

    do not show "logout"

--- tmpl/default.php
+++ tmpl/default.php
@@ -11,7 +11,8 @@ defined('_JEXEC') or die;
 
 if($client_logged_in){
     $url = JURI::current();
-    echo '<h3 id="lupo_loginlink"><a href="'.$loginlink_url.'">'.JText::_('MOD_LUPO_LOGINLINK_ACCOUNT').'</a> | <a href="'.$url.'?clientLogout"><i class="uk-icon-power-off"></i> '.JText::_('MOD_LUPO_LOGINLINK_LOGOUT').'</a></h3>';
+    echo '<h3 id="lupo_loginlink"><a href="'.$loginlink_url.'">'.JText::_('MOD_LUPO_LOGINLINK_ACCOUNT').'</a>';
+    //echo '<a href="'.$url.'?clientLogout"><i class="uk-icon-power-off"></i> '.JText::_('MOD_LUPO_LOGINLINK_LOGOUT').'</a></h3>';
 } else {
 	echo '<h3 id="lupo_loginlink"><a href="'.$loginlink_url.'">'. JText::_('MOD_LUPO_LOGINLINK_LOGIN') .'</a></h3>';
 }
