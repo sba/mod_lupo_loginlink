7430d8adb14b1a8b9ceced4a73d797f97ff26712
Stefan Bauer <sba@gmx.ch>
2018-02-01 14:41:43 +0100

    icons added

--- tmpl/default.php
+++ tmpl/default.php
@@ -11,9 +11,9 @@ defined('_JEXEC') or die;
 
 if($client_logged_in){
     $url = JURI::current();
-    echo '<h3 id="lupo_loginlink"><a href="'.$loginlink_url.'">'.JText::_('MOD_LUPO_LOGINLINK_ACCOUNT').'</a></h3>';
+    echo '<h3 id="lupo_loginlink"><a href="'.$loginlink_url.'"><i class="uk-icon-user"></i> '.JText::_('MOD_LUPO_LOGINLINK_ACCOUNT').'</a></h3>';
     //echo '<h3><a href="'.$url.'?clientLogout"><i class="uk-icon-power-off"></i> '.JText::_('MOD_LUPO_LOGINLINK_LOGOUT').'</a></h3>';
 } else {
-	echo '<h3 id="lupo_loginlink"><a href="'.$loginlink_url.'">'. JText::_('MOD_LUPO_LOGINLINK_LOGIN') .'</a></h3>';
+	echo '<h3 id="lupo_loginlink"><a href="'.$loginlink_url.'"><i class="uk-icon-sign-in"></i> '. JText::_('MOD_LUPO_LOGINLINK_LOGIN') .'</a></h3>';
 }
 
