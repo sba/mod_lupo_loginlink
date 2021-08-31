49aa154292e4dc8168c731d53f0bdcdb1a95beb4
Stefan Bauer <sba@gmx.ch>
2021-08-31 16:16:53 +0200

    option to hide client login and only show cart-link for reservations

--- tmpl/default.php
+++ tmpl/default.php
@@ -12,11 +12,15 @@ defined('_JEXEC') or die;
 $class_hidden = $nbr_reservations===0?'uk-hidden':'';
 $res = '<i class="uk-icon-shopping-cart uk-align-right '.$class_hidden.'" id="lupo_loginlink_reservations"> <span class="uk-badge uk-badge-notification">'.$nbr_reservations.'</span></i>';
 
-if($client_logged_in){
-    $url = JURI::current();
-    echo '<h3 id="lupo_loginlink"><a href="'.$loginlink_url.'"><i class="uk-icon-user"></i> '.JText::_('MOD_LUPO_LOGINLINK_ACCOUNT').$res.'</a></h3>';
-    //echo '<h3><a href="'.$url.'?clientLogout"><i class="uk-icon-power-off"></i> '.JText::_('MOD_LUPO_LOGINLINK_LOGOUT').'</a></h3>';
-} else {
-	echo '<h3 id="lupo_loginlink"><a href="'.$loginlink_url.'"><i class="uk-icon-sign-in"></i> '. JText::_('MOD_LUPO_LOGINLINK_LOGIN') .$res.'</a></h3>';
+$componentParams = JComponentHelper::getParams('com_lupo');
+if($componentParams->get('detail_show_toy_res_hide_login', '0')==0) {
+    if ($client_logged_in) {
+        $login_linktext = '<i class="uk-icon-user"></i>' .JText::_('MOD_LUPO_LOGINLINK_ACCOUNT');
+    } else {
+        $login_linktext = '<i class="uk-icon-sign-in"></i> ' . JText::_('MOD_LUPO_LOGINLINK_LOGIN');
+    }
 }
+
+?>
+<h3 id="lupo_loginlink"><a href="<?=$loginlink_url?>"><?=$login_linktext . $res ?>&nbsp;</a></h3>
 
