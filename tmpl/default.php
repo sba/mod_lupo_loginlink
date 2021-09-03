7d8ef07721d3b3dc0691fa1c02b4e204f14e4689
Stefan Bauer <sba@gmx.ch>
2021-09-03 14:00:25 +0200

    CS

--- tmpl/default.php
+++ tmpl/default.php
@@ -9,18 +9,19 @@
 
 defined('_JEXEC') or die;
 
-$class_hidden = $nbr_reservations===0?'uk-hidden':'';
-$res = '<i class="uk-icon-shopping-cart uk-align-right '.$class_hidden.'" id="lupo_loginlink_reservations"> <span class="uk-badge uk-badge-notification">'.$nbr_reservations.'</span></i>';
+$class_hidden   = $nbr_reservations === 0 ? 'uk-hidden' : '';
+$res            = '<i class="uk-icon-shopping-cart uk-align-right ' . $class_hidden . '" id="lupo_loginlink_reservations"> <span class="uk-badge uk-badge-notification">' . $nbr_reservations . '</span></i>';
+$login_linktext = '';
 
 $componentParams = JComponentHelper::getParams('com_lupo');
-if($componentParams->get('detail_show_toy_res_hide_login', '0')==0) {
-    if ($client_logged_in) {
-        $login_linktext = '<i class="uk-icon-user"></i>' .JText::_('MOD_LUPO_LOGINLINK_ACCOUNT');
-    } else {
-        $login_linktext = '<i class="uk-icon-sign-in"></i> ' . JText::_('MOD_LUPO_LOGINLINK_LOGIN');
-    }
+if ($componentParams->get('detail_show_toy_res_hide_login', '0') == 0) {
+	if ($client_logged_in) {
+		$login_linktext = '<i class="uk-icon-user"></i> ' . JText::_('MOD_LUPO_LOGINLINK_ACCOUNT');
+	} else {
+		$login_linktext = '<i class="uk-icon-sign-in"></i> ' . JText::_('MOD_LUPO_LOGINLINK_LOGIN');
+	}
 }
 
 ?>
-<h3 id="lupo_loginlink"><a href="<?=$loginlink_url?>"><?=$login_linktext . $res ?>&nbsp;</a></h3>
+<h3 id="lupo_loginlink"><a href="<?= $loginlink_url ?>"><?= $login_linktext . $res ?>&nbsp;</a></h3>
 
